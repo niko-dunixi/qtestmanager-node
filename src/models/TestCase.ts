cc20a28b5191f3bd8a849c7bcd9bb2770c0cfeb4
Corey Pyle <cpyle@mobiquityinc.com>
2016-12-30 15:54:18 -0500

    Convert to ts

---
+++ src/models/TestCase.ts
@@ -0,0 +1,46 @@
+import { QTestResource } from './QTestResource';
+import {Property} from "./Property";
+import {Link} from "./Link";
+
+export class TestCase extends QTestResource {
+    automationContent: string;
+    testCaseVersionId: number;
+    properties: Property[];
+    links: Link[];
+    id: number;
+    name: string;
+    updateURL: string;
+    private _projectId: number;
+
+	constructor() {
+		super();
+	}
+
+	get projectId() {
+		return this._projectId;
+	}
+
+	set projectId(id) {
+		this._projectId = id;
+		this._refreshURL();
+	}
+
+	toJSON() {
+		return {
+			links: this.links,
+			id: this.id,
+			name: this.name,
+			automation_content: this.automationContent,
+		};
+	}
+
+	static fromJSON(json: any) {
+        Object.create(TestCase, json);
+	}
+
+	_refreshURL() {
+		this.createURL = `/api/v3/projects/${this.projectId}/test-cases/`;
+		this.updateURL = `/api/v3/projects/${this.projectId}/test-cases/${this.id}`;
+	}
+
+}
