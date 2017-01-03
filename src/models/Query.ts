c981acc962faa5cf028d4f08127b348dc43cf6b6
Corey Pyle <cpyle@mobiquityinc.com>
2017-01-03 08:27:40 -0500

    Convert to TS

---
+++ src/models/Query.ts
@@ -0,0 +1,31 @@
+export class Query {
+    objectType: string;
+    fields: string[];
+    query: string;
+    pageSize: string;
+    page: number;
+
+
+	constructor() {
+		this.fields = ["*"];
+	}
+
+	get searchParams() {
+	    let pageSize = this.pageSize ? `pageSize=${this.pageSize}` : "";
+	    let page = this.page ? `page="${this.page}` : "";
+		return `?${pageSize}&${page}`;
+    }
+
+	addField(fieldName) {
+		if (this.fields[0] == "*") this.fields = [];
+		this.fields.push(fieldName);
+	}
+
+	toJSON() {
+		return {
+			object_type: this.objectType,
+			fields: this.fields,
+			query: this.query
+		}
+	}
+}
